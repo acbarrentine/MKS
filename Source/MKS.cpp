a2249b403ff1b0377d753185013002df2fad7c04
Alexander Barrentine <acbarrentine@gmail.com>
2018-11-03 20:43:37 -0500

    Added test scripts and updated the directory scanner to distinguish source files from others.

--- Source/MKS.cpp
+++ Source/MKS.cpp
@@ -14,14 +14,29 @@ int main(int argc, const char* argv[])
         return -1;
     }
 
-    const char* sourceDir = argv[1];
+    std::filesystem::path sourceDir(argv[1]);
+    sourceDir.make_preferred();
     std::cout << "Scanning " << sourceDir << "\n";
 
-    for(const auto& entry : std::filesystem::recursive_directory_iterator(sourceDir))
+    try
     {
-        const std::filesystem::path& path = entry.path();
-        std::cout << path << '\n';
+        for(const auto& entry : std::filesystem::recursive_directory_iterator(sourceDir))
+        {
+            if (entry.is_regular_file())
+            {
+                std::filesystem::path path = entry.path();
+                if (path.extension() == ".mks")
+                {
+                    path.make_preferred();
+                    std::cout << path << '\n';
+                }
+            }
+        }
     }
-
+    catch (std::filesystem::filesystem_error& err)
+    {
+        std::cerr << "Error " << err.code() << ": " << err.what() << "\n";
+    }
+ 
     return 0;
 }
